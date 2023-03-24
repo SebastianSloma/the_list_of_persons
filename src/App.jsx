670a9c82240aa9b49f2a07cb7d09844bd99f436b
Sebastian <sebastiansloma@gmail.com>
2023-03-24 15:39:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => <PersonList />;
+
+const PersonList = () => {
+  return <Person />;
+};
+
+const Person = () => {
+  return <div>I'm person component</div>;
+};
+
+
+export default App;
