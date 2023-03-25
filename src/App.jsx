2a06d6017b9b493626bf09e8fc9683bb85dd34e8
Sebastian <sebastiansloma@gmail.com>
2023-03-25 17:20:53 +0100

    added array with persons

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,50 @@ import './App.css';
 const App = () => <PersonList />;
 
 const PersonList = () => {
-  return <Person />;
+
+  const people =[
+    {
+      img:21,
+      name:'Johan',
+      job: 'Frontend developer'
+    },
+    {
+      img:24,
+      name:'Johnatan',
+      job: 'Backend developer'
+    },
+    {
+      img:42,
+      name:'Johannaten',
+      job: 'DevOps'
+    }
+  ]
+
+  return (
+    <section>
+
+      <Person />
+      <Person />
+      <Person />
+    </section>
+  );
 };
 
 const Person = () => {
-  return <div>I'm person component</div>;
+  const url = "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+
+  return (
+<div className='person'>
+  <img src={url} alt=""/>
+  <div>
+    <h4>name</h4>
+    <h4>job</h4>
+  </div>
+
+  </div>
+  
+  )
+
 };
 
 
